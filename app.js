c55500a5f2bba30544f48c5f0636659d9d9536d6
Mohammed Ahmed <mahme029@gold.ac.uk>
2022-10-22 14:53:57 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,139 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+let port = 3000;
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+let items = ["Milk", "Bread"];
+let WorkOutlist = ["Bench", "Squat"];
+
+app.use(express.static("public"))
+
+
+let deleteitem = ""
+
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursay", "Friday", "Saturday"]
+let daysOfTheWeek = new Date();
+let intDAy = daysOfTheWeek.getDay();
+let myday = days[intDAy]
+
+
+app.get("/", function (req, res) {
+    page1renders(req, res)
+
+
+
+})
+
+
+app.post("/", function (req, res) {
+    deleteitem = req.body.deleteWorkout
+    deleteitem1 = req.body.deleteItem;
+    item = req.body.newItem
+    workoutItem = req.body.workoutItem;
+
+    addItem(WorkOutlist, workoutItem);
+    addItem(items, item)
+
+
+    deleteitems(WorkOutlist, deleteitem);
+    deleteitems(items, deleteitem1)
+
+
+    res.redirect("/")
+})
+
+app.post("/viewss", function (req, res) {
+    page1renders(req, res)
+})
+
+
+
+
+
+
+app.get("/views", function (req, res) {
+    page2renders(req,res)
+
+})
+
+
+
+
+app.post("/views", function (req, res) {
+    res.render("page2", {
+        Date: myday,
+        Hours: daysOfTheWeek.getHours(),
+        Minutes: daysOfTheWeek.getMinutes(),
+        Seconds: daysOfTheWeek.getSeconds(),
+        items: items
+
+    })
+
+})
+
+app.post("/views2", function (req, res) {
+    let itemsinputted = req.body.items;
+    let delteditem = req.body.deleteditem;
+    addItem(items, itemsinputted)
+    deleteitems(items, delteditem)
+    res.redirect("/views")
+})
+
+
+
+
+
+
+function addItem(array, item) {
+    array.push(item)
+}
+
+
+function deleteitems(array, item) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            array.splice(i, 1)
+        }
+    }
+
+}
+
+
+function page1renders(req, res) {
+
+    res.render("page1", {
+        Fname: "Faeem",
+        Day: myday,
+        Date: daysOfTheWeek,
+        newItems: items,
+        workout: WorkOutlist
+
+    })
+
+}
+
+function page2renders(req,res){
+    res.render("page2", {
+        Date: myday,
+        Hours: daysOfTheWeek.getHours(),
+        Minutes: daysOfTheWeek.getMinutes(),
+        Seconds: daysOfTheWeek.getSeconds(),
+        items: items
+
+    })
+
+}
+
+
+
+
+app.listen(port, function () {
+    console.log("server is running on: " + port)
+})
---
+++ public/sketch.js
@@ -0,0 +1,67 @@
+let messages = document.querySelectorAll(".message")
+let body = document.querySelector("body")
+let container = document.querySelector(".container");
+let today = new Date();
+let Hours = today.getHours();
+
+let containers=document.querySelectorAll(".container")
+
+
+
+//alters the dom elemtns. if its multiple object, set teh bool array to true, next paramnet select the dom elemnt using tags or calssses, and dec is the style we want from the css.
+function messageVisibility(array, object, dec) {
+    if (array === true) {
+        let objects = document.querySelectorAll(object);
+        for (var i = 0; i < objects.length; i++) {
+            objects[i].classList.add(dec)
+        }
+
+
+    } else if (array === false) {
+        let objects = document.querySelector(object);
+        objects.classList.add(dec)
+    }
+}
+
+
+//function checks the time of sday and set the UI based oon the curretn time
+function TimeOfDay(time) {
+    if (time <= 4) {
+        body.classList.add("dark")
+        messageVisibility(true, ".message", "dark-text")
+        messageVisibility(true, ".container", "dark-border")
+        messageVisibility(true,".container","darkbg")
+
+    } else if (time >= 4 && time <= 11) {
+        body.classList.add("dull")
+        messageVisibility(true, ".message", "light-text")
+        messageVisibility(true, ".container", "light-border")
+        messageVisibility(true,".container","greybg")
+    } else if (time >= 11 && time <= 15) {
+        body.classList.add("bright")
+        messageVisibility(true, ".message", "light-text")
+        messageVisibility(true, ".container", "light-border")
+        messageVisibility(true,".container","whitebg")
+    } else if (time >= 15 && time <= 20) {
+        body.classList.add("dull")
+        messageVisibility(true, ".message", "light-text")
+        messageVisibility(true, ".container", "light-border")
+        messageVisibility(true,".container","greybg")
+    } else {
+        body.classList.add("dark")
+        messageVisibility(true, ".message", "dark-text")
+        messageVisibility(true, ".container", "dark-border")
+        messageVisibility(true,".container","darkbg")
+    }
+}
+
+
+
+TimeOfDay(Hours)
+
+messageVisibility(false, ".message", "message")
+
+messageVisibility(true, ".card", "hovering")
+
+
+
